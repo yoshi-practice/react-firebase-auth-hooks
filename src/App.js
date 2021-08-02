eb92397b7388e915abf0c7e6701a82d622df0c20
yosh1 <yoppy1125yoppy@gmail.com>
2021-08-02 13:25:27 +0900

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import SignUp from './components/SignUp';
 import { AuthProvider } from './context/AuthContext';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
     <AuthProvider>
     <div style={{margin: '2em'}}>
-      <SignUp/>
+      <BrowserRouter>
+        <Route path="/signup" component={SignUp} />
+      </BrowserRouter>
     </div>
     </AuthProvider>
   );
